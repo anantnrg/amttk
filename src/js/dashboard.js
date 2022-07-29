87f0e22b1119c56a3f78e56495cad6eda250e8c9
Anant Narayan <anantnrg@gmail.com>
2022-07-29 19:19:03 +0530

    add

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,12 +1,29 @@
 const os = require("os");
 const fs = require("fs");
 
-const root = document.documentElement;
-const cpu_percent = document.getElementById("cpu-bar");
-const ram_percent = document.getElementById("ram-bar");
-const gpu_percent = document.getElementById("gpu-bar");
-const disk_percent = document.getElementById("disk-bar");
+const cpu_usage = document.querySelector(".cpu_usage");
+const ram_usage = document.querySelector(".ram_usage");
+const gpu_usage = document.querySelector(".gpu_usage");
+const disk_usage = document.querySelector(".disk_usage");
 
-// const cpu = os.cpu
-
-cpu_percent.style.width="30%";
+function update_cpu_usage(cpu_usage, value) {
+    value = Math.round(value);
+    cpu_usage.querySelector(".cpu_usage_fill").style.width = `${value}%`;
+  }
+function update_ram_usage(ram_usage, value) {
+    value = Math.round(value);
+    ram_usage.querySelector(".ram_usage_fill").style.width = `${value}%`;
+  }
+function update_gpu_usage(gpu_usage, value) {
+    value = Math.round(value);
+    gpu_usage.querySelector(".gpu_usage_fill").style.width = `${value}%`;
+  }
+function update_disk_usage(disk_usage, value) {
+    value = Math.round(value);
+    disk_usage.querySelector(".disk_usage_fill").style.width = `${value}%`;
+  }
+/* Example */
+update_cpu_usage(cpu_usage, 55);
+update_ram_usage(ram_usage, 45);
+update_gpu_usage(gpu_usage, 24);
+update_disk_usage(disk_usage, 75)
